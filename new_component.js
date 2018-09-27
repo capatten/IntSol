7fb75bdacbacfd8c57ac9a530fe4c88d00b4e32f
capatten <pattenchada@gmail.com>
2018-09-26 21:53:31 -0600

    Added Files

---
+++ new_component.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+
+var fs = require("fs");
+
+//get provided arguments
+const [,, ...args] = process.argv
+
+function printBoom(){
+	console.log("Boom")
+}
+
+//create componentfile
+fs.writeFileSync(`./${args[0]}.jsx`, `import React from 'react'; 
+\n\n
+class ${args[1]} extends React.Component {
+\nrender() {
+\n\n}
+\n}
+` )
+
+//create componentfile
+fs.writeFileSync(`./${args[0]}.less`)
+
+//output completed message
+console.log(`Component Created`);
